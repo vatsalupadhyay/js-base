ad8eee89e55f505429da31ecaa7e0c2feb80de40
Vatsal Upadhyay <102057550+vatsalupadhyay@users.noreply.github.com>
2023-12-24 23:30:07 +0530

    Create async_notes.js

---
+++ basics/async_notes.js
@@ -0,0 +1,7 @@
+// there is a main file execution and the side handling of the code as well 
+// the code which is the part of the async code is handle in the side programming as soon as the main is executed it is checked whether the side has been completed or not if yes then it is executed 
+
+// callbacks are the functions which are executed after a certain interval of time, examples are : setTimeout , setinterval etc, this require a function as well as the time 
+//then call are the most used one of the features in js for async code, if we do not want to use then call then we can use async await for the same as well 
+
+
